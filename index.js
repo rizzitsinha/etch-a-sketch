75bd805bfdddb90b6ca7bb5ba4bf151d668abdf4
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-15 22:55:47 +0530

    Fully functioning etch-a-sketch
    The Etch-a-Sketch can now make a grid for any number of boxes

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
+const btn = document.querySelector("#btn")
+
 // Creating and appending large container
 const largeContainer = document.createElement("div");
 largeContainer.classList.add("large-container");
 document.body.appendChild(largeContainer);
 
-// Setting attributes for the large container
-largeContainer.setAttribute(`style`, `display: flex; flex-direction: column; align-items: center`)
+// Event listener to create grid
+btn.addEventListener(`click`, () => {
 
-// Creating and appending 16 containers to the large-container
-for(let i = 0; i < 16; i++){
-    const container = document.createElement("div");
-    container.classList.add("container")
-    largeContainer.appendChild(container);
-    console.log("appended container!")
+    let numberOfBoxes = Number(window.prompt("How many boxes do you wish for?", 16));
+    let boxLen = 800 / numberOfBoxes;
 
-    // Setting attributes for the container
-    container.setAttribute(`style`, `display: flex`)
+    console.log(numberOfBoxes);
+    console.log(boxLen)
+
+    createContainer(boxLen, numberOfBoxes);
+})
+
+// Function to create grid
+function createContainer(boxLen, numberOfBoxes) {
+
+    // Looping to remove previous grid
+    while(largeContainer.firstChild){
+        largeContainer.removeChild(largeContainer.lastElementChild);
+    }
     
-    // Creating and appending 16 grid boxed to each container
-    for (let i = 0; i < 16; i++) {
-        const gridBox = document.createElement("div");
-        gridBox.classList.add("grid-box");
-        container.appendChild(gridBox);
-        console.log("appended box")
-
-        // Setting attributrs for grid boxes
-        gridBox.setAttribute(`style`, `height: 50px; width: 50px; background-color: bisque; border: black 1px solid`)
-
-        // Adding hover effect
-        gridBox.addEventListener(`mouseover`, () => {
-            gridBox.style.backgroundColor = "burlywood";
-        })
-        gridBox.addEventListener(`mouseout`, () => {
-            gridBox.style.backgroundColor = "chocolate";
-        })
+    // Setting attributes for the large container
+    largeContainer.setAttribute(`style`, `display: flex; flex-direction: column; align-items: center`)
+    
+    // Creating and appending 16 containers to the large-container
+    for(let i = 0; i < numberOfBoxes; i++){
+        const container = document.createElement("div");
+        container.classList.add("container")
+        largeContainer.appendChild(container);
+    
+        // Setting attributes for the container
+        container.setAttribute(`style`, `display: flex`)
+        
+        // Creating and appending 16 grid boxed to each container
+        for (let i = 0; i < numberOfBoxes; i++) {
+            const gridBox = document.createElement("div");
+            gridBox.classList.add("grid-box");
+            container.appendChild(gridBox);
+    
+            // Setting attributrs for grid boxes
+            gridBox.setAttribute(`style`, `height: ${boxLen}px; width: ${boxLen}px; background-color: bisque; border: black 1px solid`)
+    
+            // Adding hover effect
+            gridBox.addEventListener(`mouseover`, () => {
+                gridBox.style.backgroundColor = "burlywood";
+            })
+            gridBox.addEventListener(`mouseout`, () => {
+                gridBox.style.backgroundColor = "chocolate";
+            })
+        }
     }
 }
-
 
