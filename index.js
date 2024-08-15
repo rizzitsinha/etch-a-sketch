4b14701b0519eb7b0196b008c932d6794e958eb9
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-15 14:39:46 +0530

    Functions properly as etch-a-skech

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ document.body.appendChild(largeContainer);
 // Setting attributes for the large container
 largeContainer.setAttribute(`style`, `display: flex; flex-direction: column; align-items: center`)
 
-// Creating and appending 4 containers to the large-container
-for(let i = 0; i < 4; i++){
+// Creating and appending 16 containers to the large-container
+for(let i = 0; i < 16; i++){
     const container = document.createElement("div");
     container.classList.add("container")
     largeContainer.appendChild(container);
@@ -16,15 +16,23 @@ for(let i = 0; i < 4; i++){
     // Setting attributes for the container
     container.setAttribute(`style`, `display: flex`)
     
-    // Creating and appending 4 grid boxed to each container
-    for (let i = 0; i < 4; i++) {
+    // Creating and appending 16 grid boxed to each container
+    for (let i = 0; i < 16; i++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("grid-box");
         container.appendChild(gridBox);
         console.log("appended box")
 
         // Setting attributrs for grid boxes
-        gridBox.setAttribute(`style`, `height: 200px; width: 200px; background-color: bisque; border: black 1px solid`)
+        gridBox.setAttribute(`style`, `height: 50px; width: 50px; background-color: bisque; border: black 1px solid`)
+
+        // Adding hover effect
+        gridBox.addEventListener(`mouseover`, () => {
+            gridBox.style.backgroundColor = "burlywood";
+        })
+        gridBox.addEventListener(`mouseout`, () => {
+            gridBox.style.backgroundColor = "chocolate";
+        })
     }
 }
 
